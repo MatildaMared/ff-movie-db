59b51a972685648e99521a067bd13c789048d41b
Richard Grönroos <richard.gronroos@forefront.se>
2022-11-04 16:00:39 +0100

    fixade konfigurationen för node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,19 @@
-const express = require('express') //Line 1
-const app = express() //Line 2
-const port = process.env.PORT || 5000 //Line 3
+const express = require("express"); //Line 1
+const app = express(); //Line 2
+const port = process.env.PORT || 5000; //Line 3
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)) //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get('/movies', (req: any, res: any) => {
+app.get("/api/movies", (req: any, res: any) => {
   //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }) //Line 10
-}) //
+  res.json({
+    movies: [
+      { id: 1, title: "movie1" },
+      { id: 2, title: "movie2" },
+      { id: 3, title: "movie3" },
+    ],
+  }); //Line 10
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
+}); //
